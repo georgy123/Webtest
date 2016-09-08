a2a01f02d40ffc4317f2abf2a9dcefb7abc3deb0
qzwang <2418948695@qq.com>
2016-09-08 20:35:09 +0800

    测试更新代码

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
   public static void main(String[] args) {
     System.out.println("Hello Git!");
     System.out.println("这是添加的文件");
+    System.out.println("测试是否可以更新了");
 }
 
 }
