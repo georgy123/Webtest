58df922ad40065d12a32756c0c09fe9641d65ddc
georgy wang <1030848853@qq.com>
2016-09-09 20:37:23 +0800

    add a new file

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
     System.out.println("Hello Git!");
     System.out.println("这是添加的文件");
     System.out.println("测试是否可以更新了");
+    System.out.println("hxx is a bad boy!");
 }
 
 }
